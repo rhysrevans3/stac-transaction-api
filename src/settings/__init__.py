5a9052745300741fe67059e737f7e2079b10a834
rhysrevans3 <rhys.r.evans3@gmail.com>
2026-07-17 09:25:32 +0100

    Update schema versions.

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,6 +1,7 @@
 import os
-from typing import Literal
 import re
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 if os.environ.get("TRANSACTION_AUTHORIZER") == "egi":
@@ -11,71 +12,141 @@ else:
 DEFAULT_EXTENSIONS = {
     "CMIP6": {
         "CMIP6": {
-            "regex": [r"https:\/\/esgf\.github\.io\/stac-transaction-api\/cmip6\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
-            "default": "https://esgf.github.io/stac-transaction-api/cmip6/v3.0.4/schema.json",
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/cmip6/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://esgf.github.io/stac-transaction-api/cmip6/v2.0.0/schema.json",
         },
         "alternate_assets": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
         },
         "file": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
         },
     },
     "CMIP6Plus": {
         "CMIP6Plus": {
-            "regex": [r"https:\/\/esgf\.github\.io\/stac-transaction-api\/cmip6plus\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
-            "default": "https://esgf.github.io/stac-transaction-api/cmip6plus/v1.0.4/schema.json",
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/cmip6plus/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://esgf.github.io/stac-transaction-api/cmip6plus/v2.0.1/schema.json",
         },
         "alternate_assets": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
         },
         "file": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
         },
     },
     "CMIP7": {
         "CMIP7": {
-            "regex": [r"https:\/\/esgf\.github\.io\/stac-transaction-api\/cmip7\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
-            "default": "https://esgf.github.io/stac-transaction-api/cmip7/v1.0.0/schema.json",
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/cmip7/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://esgf.github.io/stac-transaction-api/cmip7/v1.2.9/schema.json",
         },
         "alternate_assets": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
         },
         "file": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
+        },
+    },
+    "INPUT4MIP": {
+        "INPUT4MIP": {
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/input4mips/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://esgf.github.io/stac-transaction-api/input4mips/v1.2.9/schema.json",
+        },
+        "alternate_assets": {
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
+        },
+        "file": {
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://stac-extensions.github.io/file/v3.0.0/schema.json",
         },
     },
     "CORDEX-CMIP6": {
         "CORDEX-CMIP6": {
-            "regex": [r"https:\/\/esgf\.github\.io\/stac-transaction-api\/cordex-cmip6\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/cordex-cmip6/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://esgf.github.io/stac-transaction-api/cordex-cmip6/v1.2.1/schema.json",
         },
         "alternate_assets": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
         },
         "file": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
         },
     },
     "obs4MIPs": {
         "obs4MIPs": {
-            "regex": [r"https:\/\/esgf\.github\.io\/stac-transaction-api\/obs4mips\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/obs4mips/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://esgf.github.io/stac-transaction-api/obs4mips/v1.0.0/schema.json",
         },
         "alternate_assets": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
         },
         "file": {
-            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
+        },
+    },
+    "OBS4REF": {
+        "OBS4REF": {
+            "regex": [
+                r"https://esgf\.github\.io/stac-transaction-api/obs4ref/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://esgf.github.io/stac-transaction-api/obs4ref/v1.0.1/schema.json",
+        },
+        "alternate_assets": {
+            "regex": [
+                r"https://stac-extensions\.github\.io/alternate-assets/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
+            "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
+        },
+        "file": {
+            "regex": [
+                r"https://stac-extensions\.github\.io/file/v[0-9]\.[0-9]\.[0-9]/schema\.json"
+            ],
             "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
         },
     },
